1b3cd4cfc6a594e581cda717bd62c7bbacf6038e
ConfusedZK <karl_zhang@aliyun.com>
2019-03-23 14:31:15 +0800

    commit first

---
+++ northwind-parent/northwind-api/src/main/java/com/strive/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.strive.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: ApiApplication
+ * @Auther: Karl.Zhang
+ * @Date: 2019/3/23 14:26
+ * @Description: API项目启动类
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
